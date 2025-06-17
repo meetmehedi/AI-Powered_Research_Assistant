fdb49be8681789374b7a854b6d3a578e2dbbf309
MD. Mehedi Hasan <167318121+meetmehedi@users.noreply.github.com>
2025-06-17 17:58:09 +0600

    Update AI-Powered Research Assistant.py

--- AI-Powered Research Assistant.py
+++ AI-Powered Research Assistant.py
@@ -11,8 +11,8 @@ from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 
 st.set_page_config(page_title="AI-Powered Research Assistant", layout="wide")
 
-st.title("SciGenie 🧠 AI-Powered Research Assistant")
-
+st.title("SciGenie")
+st.caption("🧠 AI-Powered Research Assistant")
 # Step 1: Upload Dataset
 uploaded_file = st.file_uploader("📂 Upload your dataset (CSV/Excel)", type=["csv", "xlsx"])
 if uploaded_file:
